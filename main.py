fec147a627ee2e8746da66bf07657dcf660d7547
shanecranor <shanecranor@mines.edu>
2021-12-13 20:21:04 -0700

    cleanin up de code

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import time
 
 import cv2
-import sys
 import numpy as np
-import random
-import calibration
+from calibration import calibration
+
 
 def getCharucoBoard(x_dim, y_dim, square_size, marker_size, aruco_dict, offset):
 	board = cv2.aruco.CharucoBoard_create(x_dim,y_dim,square_size,marker_size,aruco_dict)
@@ -16,44 +15,35 @@ def getCharucoBoard(x_dim, y_dim, square_size, marker_size, aruco_dict, offset):
 
 
 def main(flags):
-	# TEST CODE TO GENERATE 5 DIFFERENT CHARUCO BOARDS
-	aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
-	# for i in range(4):
-	# 	board = getCharucoBoard(5, 7, 10, 8, aruco_dict, i)
-	# 	boardImg = board.draw((2000,2000))
-	# 	cv2.imwrite("testLarge"+str(i)+".png", boardImg)
-	# cv2.waitKey(0)
-
-	# set frame number to zero
-	frame = 0
+	frame = 0 #reset frame count
 	#get first frame
-	video_capture = cv2.VideoCapture("flower.mp4");
+	video_capture = cv2.VideoCapture("videos/demos/flower.mp4");
 	got_image, img = video_capture.read()
 	img_height, img_width = img.shape[0], img.shape[1]
-
-	# start video writer
 	if(flags['writeVideo']):
+		# start video writer
 		fourcc = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
 		videoWriter = cv2.VideoWriter("output.avi", fourcc=fourcc, fps=30.0,
 									  frameSize=(img_width, img_height))
 	linePoints = [];
-	canvasPositionVecs = [np.array([[0.0], [0.0], [0.0]]),
-						  np.array([[0.0], [0.0], [0.0]]),
-						  np.array([[0.0], [0.0], [0.0]]),
-						  np.array([[0.0], [0.0], [0.0]])]
-	tempRvec = None
+	canvasPositionVecs = [np.array([[0.0], [0.0], [0.0]])]*4
 	oldFt = 0
-	k, dist = calibration.load_coefficients('calibration_charuco.yml')
-	# linePoints.append([[[-16],[0],[95]],[0,255,0]])
-	# linePoints.append([[[-16],[0],[70]],[0,255,0]])
-	# linePoints.append([[[-16],[10],[70]],[0,255,0]])
-	# linePoints.append([[[-24],[10],[70]],[0,255,0]])
+	k, dist = calibration.load_coefficients('calibration/calibration_charuco.yml')
+	# create aruco board & dict
+	aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+	arucoParams = cv2.aruco.DetectorParameters_create()
+	LINE_COLOR = flags["color"]
+
+	if(flags["GENERATE_IMAGES"]):
+		# TEST CODE TO GENERATE 5 DIFFERENT CHARUCO BOARDS
+		for i in range(4):
+			board = getCharucoBoard(5, 7, 10, 8, aruco_dict, i)
+			boardImg = board.draw((2000,2000))
+			cv2.imwrite("testLarge"+str(i)+".png", boardImg)
+		cv2.waitKey(0)
 	while got_image:
 		ft = time.time()
 		thresh_img = convertToBinary(img)
-		# create aruco board & dict
-		aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
-		arucoParams = cv2.aruco.DetectorParameters_create()
 		#detect markers
 		corners, ids, rejected = cv2.aruco.detectMarkers(thresh_img, aruco_dict, parameters=arucoParams, cameraMatrix=k)
 		canvasVecs = [None, None, None, None]
@@ -66,39 +56,44 @@ def main(flags):
 			board = getCharucoBoard(5, 7, SQUARE_LENGTH, MARKER_LENGTH, aruco_dict, i)
 			newCorners, newIds, newRejected, recovered = cv2.aruco.refineDetectedMarkers(thresh_img, board, corners, ids,
 																						 rejected, cameraMatrix=k)
-			if ids is not None:
-				resp, charucoCorners, charucoIds = cv2.aruco.interpolateCornersCharuco(
-					newCorners, newIds, thresh_img, board, cameraMatrix=k
-				)
-				if charucoIds is not None:
-					#img = cv2.aruco.drawDetectedCornersCharuco(img, charucoCorners, charucoIds, (0, 0, 255))
-					_, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(
-						charucoCorners, charucoIds, board,
-						cameraMatrix=k, distCoeffs=None, rvec=np.array([[0.0], [0.0], [0.0]]),
-						tvec=np.array([[0.0], [0.0], [0.0]]))
-					if i == 0:
-						#
-						img = drawPyramid(img, k, rvec, tvec, (255,0,0))
-						markerPoint = tvec;
-						#linePoints.append([tvec, (0,255,0)])
-					else:
-						img = drawPyramid(img, k, rvec, tvec, (i*63,i*63,i*63))
-						canvasVecs[i] = [tvec, rvec]
-						if i == 1 and frame == 0:
-							tempRvec = rvec
+			#skip to next board if current one is invalid
+			if ids is None: continue
+			resp, charucoCorners, charucoIds = cv2.aruco.interpolateCornersCharuco(
+				newCorners, newIds, thresh_img, board, cameraMatrix=k
+			)
+			# skip to next board if current one is invalid
+			if charucoIds is None: continue
+
+			if(flags["showDebug"]):
+				img = cv2.aruco.drawDetectedCornersCharuco(img, charucoCorners, charucoIds, (0, 0, 255))
+			_, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(
+				charucoCorners, charucoIds, board,
+				cameraMatrix=k, distCoeffs=None,
+				rvec=np.array([[0.0], [0.0], [0.0]]),
+				tvec=np.array([[0.0], [0.0], [0.0]]))
+			#check if the current board is the marker
+			if i == flags['MARKER_ID']:
+				if(flags["showDebug"]):
+					img = drawPyramid(img, k, rvec, tvec, (255,0,0))
+				markerPoint = tvec;
+				#linePoints.append([tvec, (0,255,0)])
+			else:
+				if (flags["showDebug"]):
+					img = drawPyramid(img, k, rvec, tvec, (i*63,i*63,i*63))
+				canvasVecs[i] = [tvec, rvec]
+
 		if markerPoint is None:
 			linePoints.append(None)
 		else:
 			#calculate canvas position
 			if(calculateCanvasLocation(img, k, canvasVecs, canvasPositionVecs) is not None):
 				canvasLoc, canvasRot = calculateCanvasLocation(img, k, canvasVecs, canvasPositionVecs)
-				if pointIsValid(img, k, markerPoint) and pointIsValid(img, k, canvasLoc):
-					#np.array(canvasLoc, dtype="float"),"\n\n", markerPoint,
-					tempVec = markerPoint-canvasLoc
+				if pointsAreValid(img, k, [markerPoint, canvasLoc]):
 					R, _ = cv2.Rodrigues(canvasRot*-1)
-					rotatedBoi = R @ tempVec
-					linePoints.append([rotatedBoi, (0,255,0)])
-				img = drawLines(img, linePoints, canvasLoc, k, canvasRot)
+					rotated = R @ (markerPoint-canvasLoc)
+					linePoints.append([rotated, LINE_COLOR])
+		if (calculateCanvasLocation(img, k, canvasVecs, canvasPositionVecs) is not None):
+			img = drawLines(img, linePoints, canvasLoc, k, canvasRot)
 
 		if flags['showFPS']:
 			fps = 1 / (ft - oldFt)
@@ -109,7 +104,7 @@ def main(flags):
 		if flags['showBinary']:
 			cv2.imshow("binary image", thresh_img)
 		cv2.imshow("live output", img)
-		cv2.waitKey(0)
+		cv2.waitKey(1)
 		frame += 1
 		got_image, img = video_capture.read()
 	if flags['writeVideo']:
@@ -117,34 +112,40 @@ def main(flags):
 
 # eventually will use multiple canvas boards and their positions
 # and then average them to get a more stable canvas location
-# right now it just returns the position of charuco board id == 1
+# right now we just average the rotations because rotation is far more unstable than position.
+# we just use position of canvas ID 1
 def calculateCanvasLocation(img, k, canvasVecs, canvasPositionVecs):
 	out = [np.array([[0.0], [0.0], [0.0]]), np.array([[0.0], [0.0], [0.0]])]
 	count = 0;
 	for i in range(4):
-		if canvasVecs[i] is not None and pointIsValid(img, k, canvasVecs[1]):
-			print(canvasVecs[i][1],i)
-			out[0] += canvasVecs[i][0]-canvasPositionVecs[i]
-			for j in range(4):
-				diff = 0
-				if i != j and canvasVecs[j] is not None:
-					diff += np.linalg.norm(canvasVecs[i][1]-canvasVecs[j][1])
-			print("diff", diff)
-			if(diff < .5):
-				out[1] += canvasVecs[i][1]
-				count += 1
+		#skip canvas if it isn't there or isn't valid
+		if canvasVecs[i] is None or not pointIsValid(img, k, canvasVecs[1]):
+			continue
+		# out[0] += canvasVecs[i][0]-canvasPositionVecs[i]
+		# calculate the delta between the other canvases to see if there is an error
+		diff = 0
+		for j in range(4):
+			if i != j and canvasVecs[j] is not None:
+				diff += np.linalg.norm(canvasVecs[i][1]-canvasVecs[j][1])
+		#if there is an error, we disguard the bad value from the average
+		if(diff < .5):
+			out[1] += canvasVecs[i][1]
+			count += 1
 	if count == 0 or canvasVecs[3] is None:
 		return None
 	return [canvasVecs[3][0], out[1] / count]
 
 
 def pointIsValid(img, k, point):
-	if point is None:
-		return False
-	if np.linalg.norm(point) == 0.0: #or isOffScreen(img, point, k)
+	if point is None or np.linalg.norm(point) == 0.0: #or isOffScreen(img, point, k)
 		return False
 	return True
 
+def pointsAreValid(img, k, pArr):
+	for p in pArr:
+		if not pointIsValid(img, k, p):
+			return False
+	return True
 
 def isOffScreen(img, point, k):
 	axes = np.matrix([[0, 0, 0]])
@@ -168,7 +169,7 @@ def drawLines(img, pointsArr, canvasLoc, k, canvasRvec):
 			# imagePoints1, jacobian = cv2.projectPoints(np.float32(axes), canvasRvec, pointsArr[i + 1][0] + canvasLoc, k, None)
 			img = cv2.line(img,
 						   tuple(np.int32(imagePoints0).ravel()),
-						   tuple(np.int32(imagePoints1).ravel()), pointsArr[i][1], 2)
+						   tuple(np.int32(imagePoints1).ravel()), pointsArr[i][1], 5)
 
 
 
@@ -215,16 +216,20 @@ def convertToBinary(bgr_image):
 		adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
 		thresholdType=cv2.THRESH_BINARY,
 		blockSize=699,
-		C=-15
+		C=0
 	)
 	return thresh_img
 
 
 if __name__ == "__main__":
 	main({
-		'writeVideo' : False,
+		'writeVideo' : True,
 		'showBinary' : False,
-		'showFPS' : True
+		'showFPS' : False,
+		'showDebug' : True,
+		'color' : (0, 255, 0),
+		'MARKER_ID' : 0,
+		'GENERATE_IMAGES' : False
 	})
 
 
